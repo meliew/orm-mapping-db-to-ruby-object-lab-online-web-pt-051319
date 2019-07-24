b81872faf055935e2951823607b786d1f625aead
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-24 04:49:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -93,7 +93,9 @@ class Student
     LIMIT ?
     SQL
 
-    DB[:conn].execute(sql, num_students)
+    DB[:conn].execute(sql, num_students).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.first_student_in_grade_10
