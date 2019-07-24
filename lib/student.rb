aa13c213648d50766aa79dcaddd3f1bddad99cb0
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-24 04:27:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,9 +71,9 @@ class Student
   end
 
   def self.students_below_12th_grade
-    sql = <<-SQL 
-    SELECT * 
-    FROM students 
+    sql = <<-SQL
+    SELECT *
+    FROM students
     WHERE grade < 12
     SQL
     
