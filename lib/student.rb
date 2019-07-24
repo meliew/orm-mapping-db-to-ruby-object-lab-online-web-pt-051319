302915e39984ea89ae775f44ce7f0c3c07489518
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-24 04:24:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,14 +71,11 @@ class Student
   end
 
   def self.students_below_12th_grade
-    sql =
-      "SELECT *
-      FROM students
-      WHERE GRADE <12;"
-
-
+    sql = "SELECT * FROM students WHERE grade<12;"
     DB[:conn].execute(sql)
   end
+
+
 
   def self.first_X_students_in_grade_10(num)
 
